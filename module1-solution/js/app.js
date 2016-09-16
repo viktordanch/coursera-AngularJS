e5206623712aeb9d92999a3e45f6e1d49f10ab8c
viktordanch <viktor.danch@gmail.com>
2016-09-16 10:29:11 +0300

    impruve code

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -1,33 +1,35 @@
 (function () {
   'use strict';
 
-  angular.module('LunchChecker', [])
+  angular.module('LunchChecker', []).controller('LunchCheckerController', LunchCheckerController);
 
-    .controller('LunchCheckerController', function ($scope) {
-      $scope.allItems = '';
-      $scope.userMessage = {
-        type: '',
-        text: ''
-      };
-      //$scope.totalValue = 0;
-      $scope.checkLunch = function () {
-        var lunchItemsArray = $scope.allItems.split(',');
-        var checkedArray = [];
+  LunchCheckerController.$inject = ['$scope'];
 
-        lunchItemsArray.forEach(function (item) {
-          if (item.replace(/ /, '').length > 0) {
-            checkedArray.push(item);
-          }
-        });
+  function LunchCheckerController ($scope) {
+    $scope.allItems = '';
+    $scope.userMessage = {
+      type: '',
+      text: ''
+    };
+    
+    $scope.checkLunch = function () {
+      var lunchItemsArray = $scope.allItems.split(',');
+      var checkedArray = [];
 
-        if (checkedArray.length == 0) {
-          $scope.userMessage = { text: 'Please enter data first', status: 'error' };
+      lunchItemsArray.forEach(function (item) {
+        if (item.replace(/ /, '').length > 0) {
+          checkedArray.push(item);
         }
-        else if (checkedArray.length < 4) {
-          $scope.userMessage = { text: 'Enjoy!', status: 'success' };
-        } else if (checkedArray.length > 3) {
-          $scope.userMessage = { text: 'Too much!', status: 'success' };
-        }
-      };
-    });
+      });
+
+      if (checkedArray.length == 0) {
+        $scope.userMessage = { text: 'Please enter data first', status: 'error' };
+      }
+      else if (checkedArray.length < 4) {
+        $scope.userMessage = { text: 'Enjoy!', status: 'success' };
+      } else if (checkedArray.length > 3) {
+        $scope.userMessage = { text: 'Too much!', status: 'success' };
+      }
+    };
+  };
 })();
